6f3b1770d69fb5827d7039bd0d6c617cd034ca97
-Bruno Arthsson <arthzzon@gmail.com>
2023-08-29 11:23:15 -0300

    update ceaser

---
+++ modulo_2/ceasar/ceasar.c
@@ -0,0 +1,49 @@
+﻿#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) { // recebe a key antes da execução do programa na chamada da função principal, pode ser feito a patir do prompt - ceasar.exe 10
+  
+    if (argc != 2) { //verifica a quantidade de argumentps
+        printf("Usage: %s key\n", argv[0]); //argv[0] retorna o path
+        return 1;
+    }
+
+    
+    char *key = argv[1];
+    for (int i = 0; i < strlen(key); i++) {
+        if (!isdigit(key[i])) {
+            printf("Usage: %s key\n", argv[0]);
+            return 1;
+        }
+    }
+
+    
+    int k = atoi(key); //passando para inteiro
+
+    char plaintext[1000];
+    printf("plaintext: ");
+    fgets(plaintext, sizeof(plaintext), stdin);
+
+    printf("ciphertext: ");
+
+    for (int i = 0, n = strlen(plaintext); i < n; i++) {
+        if (isalpha(plaintext[i])) {
+            char base = isupper(plaintext[i]) ? 'A' : 'a';
+            printf("%c \n", (plaintext[i] - base + k) % 26 + base);
+			printf("%c \n",(plaintext[i] - base + k));
+			printf("%c \n",(plaintext[i] - base + k)%26);
+			printf("%c \n",plaintext[i]);
+			printf("%d \n", k);
+			printf("%c \n", base);
+        } else {
+            printf("%c", plaintext[i]);
+        }
+    }
+
+    printf("\n");
+
+    return 0;
+}
+
---
+++ modulo_2/readability/exercicio_1.c
@@ -0,0 +1,64 @@
+﻿#include <stdio.h> 
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+#include <stdbool.h>
+
+int count_letters(const char *text) { //* indica que a variavel vai guardar um endereço e não um valor (ponteiro)
+    int count = 0;
+    for (int i = 0; text[i] != '\0'; i++) { // \0 é colocado na string(char) para identificar o fim da string(char) 
+        if (isalpha(text[i])) {
+            count++;
+        }
+    }
+    return count;
+}
+
+int count_words(const char *text) {
+    int count = 1; 
+    for (int i = 0; text[i] != '\0'; i++) {
+        if (isblank(text[i])) {
+            count++;
+        }
+    }
+    return count;
+}
+
+int count_sentences(const char *text) {
+    int count = 0;
+    for (int i = 0; text[i] != '\0'; i++) {
+        if (text[i] == '.' || text[i] == '!' || text[i] == '?') {
+            count++;
+        }
+    }
+    return count;
+}
+
+int calculate_index(int letters, int words, int sentences) {
+    float L = (float) letters / words * 100;
+    float S = (float) sentences / words * 100;
+    return (int)round(0.0588 * L - 0.296 * S - 15.8);
+}
+
+int main(void) {
+    char text[1000];
+	
+    printf("Text: ");
+    fgets(text, sizeof(text), stdin);
+
+    int letters = count_letters(text);
+    int words = count_words(text);
+    int sentences = count_sentences(text);
+
+    int index = calculate_index(letters, words, sentences);
+
+    if (index >= 16) {
+        printf("Grade 16+\n");
+    } else if (index < 1) {
+        printf("Before Grade 1\n");
+    } else {
+        printf("Grade %d\n", index);
+    }
+
+    return 0;
+}
